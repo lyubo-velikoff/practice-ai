ba71d5bd60a5c2dfbe17fbde37fa87537e349d9e
Lyubomir Velikov <lyubo.velikoff@gmail.com>
2024-05-05 18:11:58 +0100

    Add shared UI lib

---
+++ libs/shared/ui/src/lib/shared-ui.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable-next-line */
+export interface SharedUiProps {}
+
+export function SharedUi(props: SharedUiProps) {
+  return <h1 className="text-white bg-black">Welcome to SharedUi!</h1>;
+}
+
+export default SharedUi;
