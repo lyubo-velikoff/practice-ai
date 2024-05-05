8d4063cd92c2abdf3173211416bed7b62361f886
Lyubomir Velikov <lyubo.velikoff@gmail.com>
2024-05-05 22:12:50 +0100

    Update not-found.tsx

--- apps/blog/src/pages/not-found.tsx
+++ apps/blog/src/pages/not-found.tsx
@@ -5,9 +5,9 @@ const NotFound: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       {/* Centered content */}
-      <h1 className="text-5xl font-bold text-red-600">404</h1>{' '}
+      <h1 className="text-5xl font-bold text-red-600">404</h1>
       {/* Error code */}
-      <p className="mt-4 text-xl text-gray-700">Page Not Found</p>{' '}
+      <p className="mt-4 text-xl text-gray-700">Page Not Found</p>
       {/* Error message */}
       <p className="mt-2 text-gray-500">
         Sorry, the page you're looking for doesn't exist.
