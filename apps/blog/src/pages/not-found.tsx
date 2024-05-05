e2d76b36df4531fd1d4514e55f639395aef439a9
Lyubomir Velikov <lyubo.velikoff@gmail.com>
2024-05-05 22:12:06 +0100

    Update not-found.tsx

--- apps/blog/src/pages/not-found.tsx
+++ apps/blog/src/pages/not-found.tsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center"> {/* Centered content */}
-      <h1 className="text-5xl font-bold text-red-600">404</h1> {/* Error code */}
-      <p className="mt-4 text-xl text-gray-700">Page Not Found</p> {/* Error message */}
-      <p className="mt-2 text-gray-500">Sorry, the page you're looking for doesn't exist.</p>
-      <Link to="/" className="px-4 py-2 mt-6 text-white bg-blue-600 rounded hover:bg-blue-700"> {/* Back to Home */}
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      {/* Centered content */}
+      <h1 className="text-5xl font-bold text-red-600">404</h1>{' '}
+      {/* Error code */}
+      <p className="mt-4 text-xl text-gray-700">Page Not Found</p>{' '}
+      {/* Error message */}
+      <p className="mt-2 text-gray-500">
+        Sorry, the page you're looking for doesn't exist.
+      </p>
+      <Link
+        to="/"
+        className="px-4 py-2 mt-6 text-white bg-gray-900 rounded hover:bg-gray-700"
+      >
+        {/* Back to Home */}
         Go Back Home
       </Link>
     </div>
