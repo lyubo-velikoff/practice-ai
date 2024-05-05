e208f629a835cbb165db81bb12ec4191d8939211
Lyubomir Velikov <lyubo.velikoff@gmail.com>
2024-05-05 20:44:14 +0100

    Add api and update styles

--- apps/blog/src/app/app.tsx
+++ apps/blog/src/app/app.tsx
@@ -1,40 +1,27 @@
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../layouts/header';
 import Footer from '../layouts/footer';
 import About from '../pages/about';
-import Home from '../pages/home';
 import NotFound from '../pages/not-found';
-import BlogPostPage from '../pages/blog-post';
+import BlogPostDetail from '../pages/blog-post';
+import BlogPostsContainer from '../containers/blog-post';
 
 const App: React.FC = () => {
-  const blogPosts = [
-    {
-      id: 1,
-      title: 'Introduction to React',
-      excerpt: 'Learn the basics of React and how to get started.',
-      author: 'John Doe',
-      date: '2024-05-05',
-    },
-    {
-      id: 2,
-      title: 'Getting Started with TypeScript',
-      excerpt: 'A quick guide to understanding TypeScript.',
-      author: 'Jane Smith',
-      date: '2024-05-06',
-    },
-    // Additional blog posts can be added here
-  ];
-
   return (
     <Router>
       <Header /> {/* Include Header component */}
-      <div className="container flex-1 p-4 mx-auto"> {/* Main content area */}
-        <Routes> {/* Define routes for different pages */}
-          <Route path="/" element={<Home posts={blogPosts} />} /> {/* Home page */}
+      <div className="container flex-1 p-4 mx-auto">
+        {' '}
+        {/* Main content area */}
+        <Routes>
+          {' '}
+          {/* Define routes for different pages */}
+          <Route path="/" element={<BlogPostsContainer />} /> {/* Home page */}
           <Route path="/about" element={<About />} /> {/* About page */}
-          <Route path="/post/:postId" element={<BlogPostPage posts={blogPosts} />} /> {/* Blog Post page */}
-          <Route path="*" element={<NotFound />} /> {/* Not Found page for unmatched routes */}
+          <Route path="/post/:id" element={<BlogPostDetail />} />{' '}
+          {/* Blog post detail by slug */}
+          <Route path="*" element={<NotFound />} />{' '}
+          {/* Not Found page for unmatched routes */}
         </Routes>
       </div>
       <Footer /> {/* Include Footer component */}
